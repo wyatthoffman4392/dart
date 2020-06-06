d0561e2963a7927cd7e94d0d7c00fdac81d5b647
Wyatt <wyatthoffman1@Wyatts-MacBook-Pro.local>
2020-06-06 08:34:44 -0700

    Added constructors to Dog class

--- Object Oriented/dog_class.dart
+++ Object Oriented/dog_class.dart
@@ -2,29 +2,36 @@
 // Author: Wyatt Hoffman, Date: June 6, 2020
 
 void main() {
-  var dog1 = Dog();   // dog1 is a reference variable to the instance of the Dog object
-  dog1.id = 1;
-  dog1.name = 'Spot';
+  var dog1 = Dog(1, 'Spot'); // dog1 is a reference variable to the instance of the Dog object, called by the Dog Constructor
 
   dog1.sleep();
   dog1.eat();
 
-  var dog2 = Dog();   // dog2 is a reference variable to the instance of the Dog object
-  dog2.id = 2;
-  dog2.name = 'Jack';
+  var dog2 = Dog(2, 'Jack'); // dog2 is a reference variable to the instance of the Dog object
 
   dog2.sleep();
   dog2.eat();
+
+  var dog3 = Dog.namedConstructor(3, 'Oreo');
+  print('${dog3.id} and ${dog3.name}');
 }
 
 // Define properties and methods of a dog
 class Dog {
   // Instance Variables, defaults to null unless a default is specified
-  int id = 0;   // id is defaulted to 0 instead of null
+  int id = 0; // id is defaulted to 0 instead of null
   String name;
   int age;
   String type;
   List<String> favoriteFoods;
+
+
+  // Dog() {
+  //  print('This is the default constructor');
+  // }
+
+  Dog(this.id, this.name); // Parameterized Constructor
+  Dog.namedConstructor(this.id, this.name); // Named Constructor
 
   // Methods
   // "this" is used to refer to the current instance of the object
