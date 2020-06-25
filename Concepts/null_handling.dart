1eba62766788d919d7217839b0439f4a12bc9155
Wyatt <wyatthoffman4392@gmail.com>
2020-06-25 14:54:49 -0600

    Added a demonstration of Null Handling with String Interpolation to see if it was possible

--- Concepts/null_handling.dart
+++ Concepts/null_handling.dart
@@ -3,14 +3,17 @@
 // Author: Wyatt Hoffman, Date: June 16, 2020
 
 void main() {
-  Person person = null;
-  String place = null;
+  Person person = Person('John');
+  Person person2 = Person(null);
 
   // Null Operator: ?? can be used with variables
-  print(place ?? '-');
+  // print(place ?? '-');
 
   // Null Operator: ? can be used with constructors
-  print(person?.name ?? '-');
+  print(person?.name ?? 'N/A');
+
+  // Null Operator ?. and ?? can be used with String interpolation
+  print('${person2?.name ?? 'N/A'}');
 }
 
 class Person {
